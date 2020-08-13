a4452d26b468c06aab83a606c49b91a11e2f03c7
Patrick James Mardis <59069682+patrickjamesmardis@users.noreply.github.com>
2020-08-13 09:59:51 -0400

    update main.js - fix catch conditional

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
 let form = document.querySelector("form");
 form.addEventListener("submit", function(e) {
-  checkThis(e);
-  e.preventDefault();
+    checkThis(e);
+    e.preventDefault();
 });
 
 
@@ -36,91 +35,92 @@ const grabContent = async function() {
 let descTag = document.querySelector('h2#descText');
 
 function getImage() {
-  grabContent().then(data => {
-    descriptionText = data.alt_description;
-    randImgId = data.id;
-    photoURL = data.urls.thumb;
-    descTag.innerHTML = descriptionText;
-  }).catch(function() {
-    cIDIndex++;
-    URL = `https://api.unsplash.com/photos/random?collections=${collectionID}&client_id=${clientIDS[cIDIndex]}`;
-    getImage();
+    grabContent().then(data => {
+        descriptionText = data.alt_description;
+        randImgId = data.id;
+        photoURL = data.urls.thumb;
+        descTag.innerHTML = descriptionText;
+    }).catch(function() {
+        cIDIndex++;
+        if (cIDIndex >= clientIDS.length) {
+            swal({
+                title: "Sorry!",
+                text: "It looks like we've used all of our Unsplash API calls for this hour. Try again in a bit.",
+                button: false,
+                icon: "error"
+            });
+        }
+        URL = `https://api.unsplash.com/photos/random?collections=${collectionID}&client_id=${clientIDS[cIDIndex]}`;
+        getImage();
 
-    if(cIDIndex >= clientIDS.length) {
-      swal({
-        title: "Sorry!",
-        text: "It looks like we've used all of our Unsplash API calls for this hour. Try again in a bit.",
-        button: false,
-        icon: "error"
-      });
-    }
-  });
+
+    });
 }
 
 function checkThis(event) {
     userInputId = event.srcElement[0].value.split('/').pop();
     inputTag.value = "";
     if (userInputId == randImgId) {
-      console.log(photoURL);
-      let newImg = document.createElement('img');
-      newImg.setAttribute('src', photoURL);
-      pizzaTag.appendChild(newImg);
-      currentTopping++;
-      if(currentTopping != numToppings) {getImage();}
-      if(currentTopping == numToppings) {
-        swal({
-          title: "Congrats!",
-          text: "You found all the ingredients to make the cat's pizza!",
-          icon: "success",
-          button: "Next"
-        }).then(value => {
-          if(value) {
+        console.log(photoURL);
+        let newImg = document.createElement('img');
+        newImg.setAttribute('src', photoURL);
+        pizzaTag.appendChild(newImg);
+        currentTopping++;
+        if (currentTopping != numToppings) { getImage(); }
+        if (currentTopping == numToppings) {
             swal({
-              text: "Ready to play again?", 
-              button: "Yes!"
-            }).then(val => {
-              if(val) {
-                location.reload();
-              }
+                title: "Congrats!",
+                text: "You found all the ingredients to make the cat's pizza!",
+                icon: "success",
+                button: "Next"
+            }).then(value => {
+                if (value) {
+                    swal({
+                        text: "Ready to play again?",
+                        button: "Yes!"
+                    }).then(val => {
+                        if (val) {
+                            location.reload();
+                        }
+                    });
+                }
             });
-          }
-        });
-      }
+        }
     } else {
-      setTimeout(function(){
-        swal({
-          title: "Oops!",
-          text: "Looks like you grabbed the wrong ingredient.",
-          icon: "error",
-          button: "Try again"
-        });
-      }, 1)
-      
+        setTimeout(function() {
+            swal({
+                title: "Oops!",
+                text: "Looks like you grabbed the wrong ingredient.",
+                icon: "error",
+                button: "Try again"
+            });
+        }, 1)
+
     }
 }
 
 
-document.querySelector("span.start").addEventListener("click", function () {
+document.querySelector("span.start").addEventListener("click", function() {
     catTag.classList.add("animate");
     setTimeout(function() {
-      swal({
-        title: "Uh oh!",
-        text: "The cat starved waiting on their pizza",
-        icon: "error",
-        button: "Try again"
-      }).then(value => {
-        if(value) {
-          location.reload();
-        }
-      })
+        swal({
+            title: "Uh oh!",
+            text: "The cat starved waiting on their pizza",
+            icon: "error",
+            button: "Try again"
+        }).then(value => {
+            if (value) {
+                location.reload();
+            }
+        })
     }, 150000);
     startTextTag.style.opacity = 0;
     pizzaTag.style.display = "block";
     inputTag.style.display = "initial";
-    setTimeout(function(){ 
-      startTextTag.style.display = "none"; 
-      pizzaTag.style.opacity = 1;
-      inputTag.style.opacity = 1;
-      getImage();
+    setTimeout(function() {
+        startTextTag.style.display = "none";
+        pizzaTag.style.opacity = 1;
+        inputTag.style.opacity = 1;
+        getImage();
     }, 300);
 })
